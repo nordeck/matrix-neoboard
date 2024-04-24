6ca01eea2795a3307c272200eef97ba976a03c1f
Milton Moura <miltonmoura@gmail.com>
2024-04-24 15:34:43 +0000

    Makes Droppable from react-beautiful-dnd work with React 18
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>

---
+++ src/components/SlideOverviewBar/StrictModeDroppable.tsx
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 Nordeck IT + Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Hack to make React 18 StrictMode work with react-beautiful-dnd
+// from https://github.com/atlassian/react-beautiful-dnd/issues/2399#issuecomment-1167427762
+
+import { useEffect, useState } from 'react';
+import { Droppable, DroppableProps } from 'react-beautiful-dnd';
+
+export const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (!enabled) {
+    return null;
+  }
+
+  return <Droppable {...props}>{children}</Droppable>;
+};
