b26cda6bfb5722fdd0643cc31684cf0bbfeeaa24
Kim Brose <kim.brose@nordeck.net>
2024-08-02 15:03:25 +0200

    paint element borders individually
    Signed-off-by: Kim Brose <kim.brose@nordeck.net>

--- packages/react-sdk/src/components/Whiteboard/ElementBehaviors/Selection/ElementBorder.tsx
+++ packages/react-sdk/src/components/Whiteboard/ElementBehaviors/Selection/ElementBorder.tsx
@@ -89,16 +89,48 @@ export function ElementBorder({ elementIds, padding = 1 }: ElementBorderProps) {
       {isInSelectionMode && (
         <g>
           {!lineRenderProperties && (
-            <rect
-              data-testid={`${elementIds[0]}-border`}
-              fill="transparent"
-              height={selectionHeight}
-              stroke={theme.palette.primary.main}
-              strokeWidth={selectionBorderWidth}
-              width={selectionWidth}
-              x={selectionX}
-              y={selectionY}
-            />
+            <>
+              <line
+                key={`element-${elementIds[0]}-border-south`}
+                fill="none"
+                stroke={theme.palette.primary.main}
+                strokeWidth={selectionBorderWidth}
+                x1={selectionX}
+                x2={selectionX + selectionWidth}
+                y1={selectionY}
+                y2={selectionY}
+              />
+              <line
+                key={`element-${elementIds[0]}-border-east`}
+                fill="none"
+                stroke={theme.palette.primary.main}
+                strokeWidth={selectionBorderWidth}
+                x1={selectionX + selectionWidth}
+                x2={selectionX + selectionWidth}
+                y1={selectionY}
+                y2={selectionY + selectionHeight}
+              />
+              <line
+                key={`element-${elementIds[0]}-border-north`}
+                fill="none"
+                stroke={theme.palette.primary.main}
+                strokeWidth={selectionBorderWidth}
+                x1={selectionX + selectionWidth}
+                x2={selectionX}
+                y1={selectionY + selectionHeight}
+                y2={selectionY + selectionHeight}
+              />
+              <line
+                key={`element-${elementIds[0]}-border-west`}
+                fill="none"
+                stroke={theme.palette.primary.main}
+                strokeWidth={selectionBorderWidth}
+                x1={selectionX}
+                x2={selectionX}
+                y1={selectionY + selectionHeight}
+                y2={selectionY}
+              />
+            </>
           )}
           {lineRenderProperties ? (
             <>
