97fc138b07b292b4a68ff9571830afba8d9f2e22
Marcel <MTRNord@users.noreply.github.com>
2024-08-07 08:53:50 +0200

    Remove the "REACT_APP_IMAGES" option (#490)
    * Remove the "REACT_APP_IMAGES" option
    * Ensure we actually test for the upload image button to exist rather than for it to not exist now that we expect it to always exist.
    * Ensure the button role is used which is the correct role for this button despite the dropzone under it
    * Fix formatting of ToolsBar.tsx
    * Fix typoed role selector in test
    
    ---------
    
    Signed-off-by: MTRNord <marcel.radzio@nordeck.net>

--- packages/react-sdk/src/components/ToolsBar/ToolsBar.tsx
+++ packages/react-sdk/src/components/ToolsBar/ToolsBar.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import { getEnvironment } from '@matrix-widget-toolkit/mui';
 import CircleIcon from '@mui/icons-material/Circle';
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
 import NorthEastIcon from '@mui/icons-material/NorthEast';
@@ -33,8 +32,6 @@ import { UploadIcon } from '../icons/UploadIcon';
 import { useSlideImageUpload } from './useSlideImageUpload';
 
 export function ToolsBar() {
-  const imageUploadEnabled =
-    getEnvironment('REACT_APP_IMAGES', 'false') === 'true';
   const { t } = useTranslation();
   const isLocked = useSlideIsLocked();
   const { activeTool, setActiveTool } = useLayoutState();
@@ -114,16 +111,18 @@ export function ToolsBar() {
             onChange={handleRadioClick}
           />
         ))}
-        {imageUploadEnabled && (
-          <ToolbarButton
-            aria-label={t('toolsBar.imageUploadTool', 'Upload image')}
-            disabled={isLocked}
-            {...getRootProps()}
-          >
-            <input {...getInputProps()} onClick={undefined} />
-            <UploadIcon sx={{ height: 22 }} />
-          </ToolbarButton>
-        )}
+        <ToolbarButton
+          aria-label={t('toolsBar.imageUploadTool', 'Upload image')}
+          disabled={isLocked}
+          {...getRootProps()}
+          // This must be button and it MUST be after getRootProps as the dropzone would otherwise set it to the "presentation" role
+          // However in this case we want it to be a button
+          role="button"
+        >
+          {/* We are hiding it for screen readers and instead expect the button to be used */}
+          <input aria-hidden={true} {...getInputProps()} onClick={undefined} />
+          <UploadIcon sx={{ height: 22 }} />
+        </ToolbarButton>
       </ToolbarRadioGroup>
     </Toolbar>
   );
