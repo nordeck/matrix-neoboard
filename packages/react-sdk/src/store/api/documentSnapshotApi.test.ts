efd33cc3e6d8f2315b0c882c1a5297b33d484ba8
MTRNord <mtrnord1@gmail.com>
2024-10-16 12:25:51 +0200

    Fix types
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- packages/react-sdk/src/store/api/documentSnapshotApi.test.ts
+++ packages/react-sdk/src/store/api/documentSnapshotApi.test.ts
@@ -407,17 +407,14 @@ describe('createDocumentSnapshot', () => {
 
   it('should accept failing chunks', async () => {
     widgetApi.sendRoomEvent
-      .mockImplementationOnce(
-        // @ts-expect-error - ignore the type error
-        async (type: string, content: object) => ({
-          type,
-          content,
-          event_id: '$snapshot-event-id',
-          origin_server_ts: 0,
-          room_id: '!room-id',
-          sender: '@user-id',
-        }),
-      )
+      .mockImplementationOnce(async <T>(type: string, content: T) => ({
+        type,
+        content,
+        event_id: '$snapshot-event-id',
+        origin_server_ts: 0,
+        room_id: '!room-id',
+        sender: '@user-id',
+      }))
       .mockRejectedValueOnce(new Error('Timeout'));
 
     const store = createStore({ widgetApi });
