198cb9e394450985b01331ddd45b526850c9c54f
Christian Paul <christian.paul@nordeck.net>
2025-01-28 21:58:39 +0100

    WIP: LLM integration
    Signed-off-by: Christian Paul <christian.paul@nordeck.net>

--- packages/react-sdk/src/components/Whiteboard/ElementBehaviors/Text/TextElement.tsx
+++ packages/react-sdk/src/components/Whiteboard/ElementBehaviors/Text/TextElement.tsx
@@ -18,7 +18,11 @@ import { styled } from '@mui/material';
 import { useCallback, useEffect, useState } from 'react';
 import { useUnmount } from 'react-use';
 import { findForegroundColor } from '../../../../lib';
-import { TextAlignment, useWhiteboardSlideInstance } from '../../../../state';
+import {
+  PathElement,
+  TextAlignment,
+  useWhiteboardSlideInstance,
+} from '../../../../state';
 import { TextEditor } from './TextEditor';
 
 export type ForeignObjectNoInteractionProps = {
@@ -92,6 +96,99 @@ export const TextElement = ({
     }
   }, [elementId, slideInstance, text, unsubmittedText]);
 
+  const handleAICompletion = useCallback(async () => {
+    const allElements = Object.values(
+      slideInstance.getElements(
+        slideInstance.getElementIds().filter((id) => id !== elementId),
+      ),
+    );
+    // Find all incoming lines
+    const incomingConnections = allElements.filter((element) => {
+      if (
+        element.type !== 'path' ||
+        element.endMarker !== 'arrow-head-line' ||
+        element.kind !== 'line'
+      ) {
+        return false;
+      }
+      const endX = element.position.x + element.points[1].x;
+      const endY = element.position.y + element.points[1].y;
+      return endX > x && endY > y && endX < x + width && endY < y + height;
+    }) as PathElement[];
+    // Find all shapes which are the origin of an incoming line
+    const incomingShapes = allElements.filter((element) => {
+      if (element.type !== 'shape' || !element.text) {
+        return false;
+      }
+      for (const line of incomingConnections) {
+        if (
+          line.position.x + line.points[0].x > element.position.x &&
+          line.position.y + line.points[0].y > element.position.y &&
+          line.position.x + line.points[0].x <
+            element.position.x + element.width &&
+          line.position.y + line.points[0].y <
+            element.position.y + element.height
+        ) {
+          return true;
+        }
+      }
+    });
+    // If there are no incoming shapes, give the user a hint.
+    if (incomingShapes.length === 0) {
+      slideInstance.updateElement(elementId, {
+        text: 'Use arrows to point from other shapes to this one. Then press the robot for an AI message.',
+      });
+      return;
+    }
+    // Sort the shapes from left to right, then top to bottom to pretend the AI reads the page.
+    incomingShapes
+      .sort((a, b) => a.position.x - b.position.x)
+      .sort((a, b) => a.position.y - b.position.y);
+    console.log(incomingShapes);
+    const elementWithModelStatement = incomingShapes.find(
+      (element) =>
+        element.type === 'shape' && element.text.startsWith('model: '),
+    );
+    const model =
+      (elementWithModelStatement?.type === 'shape'
+        ? elementWithModelStatement.text.slice('model: '.length).trim()
+        : '') || 'llama3.2';
+    const prompt = incomingShapes
+      .map((element) =>
+        element.type === 'shape' && !element.text.startsWith('model: ')
+          ? element.text.trim()
+          : '',
+      )
+      .join('\n\n');
+    slideInstance.updateElement(elementId, {
+      text: '...',
+    });
+    // Ask the AI
+    const res = await fetch('http://localhost:11434/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        model,
+        prompt,
+        stream: false,
+      }),
+    });
+    if (!res.ok) {
+      slideInstance.updateElement(elementId, {
+        text: `HTTP error ${res.status}`,
+      });
+      return;
+    }
+    const data = await res.json();
+    console.log(data);
+    const text = data.response.replace(/<think>.*?<\/think>\W*/gs, '');
+    slideInstance.updateElement(elementId, {
+      text,
+    });
+  }, [elementId, slideInstance, x, y, width, height]);
+
   // If text editing is exited before the blur is received force a submit
   useUnmount(handleBlur);
 
@@ -108,22 +205,34 @@ export const TextElement = ({
   }
 
   return (
-    <ForeignObjectNoInteraction x={x} y={y} height={height} width={width}>
-      <TextEditor
-        color={color}
-        content={unsubmittedText}
-        contentAlignment={textAlignment}
-        contentBold={textBold}
-        contentItalic={textItalic}
-        editModeOnMount={editModeOnMount}
-        editable={active}
-        onBlur={handleBlur}
-        onChange={handleTextChange}
-        height={height}
-        width={width}
-        fontSize={fontSize}
-        setTextToolsEnabled={setTextToolsEnabled}
-      />
-    </ForeignObjectNoInteraction>
+    <>
+      <ForeignObjectNoInteraction x={x} y={y} height={height} width={width}>
+        <TextEditor
+          color={color}
+          content={unsubmittedText}
+          contentAlignment={textAlignment}
+          contentBold={textBold}
+          contentItalic={textItalic}
+          editModeOnMount={editModeOnMount}
+          editable={active}
+          onBlur={handleBlur}
+          onChange={handleTextChange}
+          height={height}
+          width={width}
+          fontSize={fontSize}
+          setTextToolsEnabled={setTextToolsEnabled}
+        />
+      </ForeignObjectNoInteraction>
+      <text
+        x={x + width + 16}
+        y={y + 16}
+        height={32}
+        width={32}
+        fontSize={28}
+        onClick={handleAICompletion}
+      >
+        🤖
+      </text>
+    </>
   );
 };
