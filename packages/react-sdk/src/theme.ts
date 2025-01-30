34f9097fe3c520785d38931fbf80995411f77866
Ahmad Kadri <ahmad.kadri@nordeck.net>
2025-01-30 14:59:20 +0100

    add import to the theme.ts
    Signed-off-by: Ahmad Kadri <ahmad.kadri@nordeck.net>

--- packages/react-sdk/src/theme.ts
+++ packages/react-sdk/src/theme.ts
@@ -15,64 +15,72 @@
  */
 
 import { createTheme } from '@mui/material/styles';
+import AbelRegular from './fonts/Abel-Regular.ttf';
+import ActorRegular from './fonts/Actor-Regular.ttf';
+import AdaminaRegular from './fonts/Adamina-Regular.ttf';
+import ChewyRegular from './fonts/Chewy-Regular.ttf';
+import GwendolynRegular from './fonts/Gwendolyn-Regular.ttf';
+import InterRegular from './fonts/Inter_24pt-Regular.ttf';
+import PirataOneRegular from './fonts/PirataOne-Regular.ttf';
 
 const theme = createTheme({
   typography: {
-    fontFamily: 'Abel, Actor, Adamina, Chewy, Gwendolyn, Inter, Pirata One',
+    fontFamily:
+      'Abel, Actor, Adamina, Chewy, Gwendolyn, Inter, Pirata One, sans-serif',
   },
   components: {
     MuiCssBaseline: {
       styleOverrides: `
-                @font-face {
-                    font-family: 'Abel';
-                    src: local('Abel'), url('/fonts/Abel-Regular.ttf') format('truetype');
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                }
-                @font-face {
-                    font-family: 'Actor';
-                    src: local('Actor'), url('/fonts/Actor-Regular.ttf') format('truetype');
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                }
-                @font-face {
-                    font-family: 'Adamina';
-                    src: local('Adamina'), url('/fonts/Adamina-Regular.ttf') format('truetype');
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                }
-                @font-face {
-                    font-family: 'Chewy';
-                    src: local('Chewy'), url('/fonts/Chewy-Regular.ttf') format('truetype');
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                }
-                @font-face {
-                    font-family: 'Gwendolyn';
-                    src: local('Gwendolyn'), url('/fonts/Gwendolyn-Regular.ttf') format('truetype');
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                }
-                @font-face {
-                    font-family: 'Inter';
-                    src: local('Inter'), url('/fonts/Inter-Regular.ttf') format('truetype');
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                }
-                @font-face {
-                    font-family: 'Pirata One';
-                    src: local('Pirata One'), url('/fonts/PirataOne-Regular.ttf') format('truetype');
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                }
-            `,
+        @font-face {
+          font-family: 'Abel';
+          src: local('Abel'), url(${AbelRegular}) format('truetype');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Actor';
+          src: local('Actor'), url(${ActorRegular}) format('truetype');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Adamina';
+          src: local('Adamina'), url(${AdaminaRegular}) format('truetype');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Chewy';
+          src: local('Chewy'), url(${ChewyRegular}) format('truetype');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Gwendolyn';
+          src: local('Gwendolyn'), url(${GwendolynRegular}) format('truetype');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Inter';
+          src: local('Inter'), url(${InterRegular}) format('truetype');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+        @font-face {
+          font-family: 'Pirata One';
+          src: local('Pirata One'), url(${PirataOneRegular}) format('truetype');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+      `,
     },
   },
 });
