04006fb139a1543432254e4ad9ab4924fb9c8a8d
Ahmad Kadri <ahmad.kadri@nordeck.net>
2025-03-17 15:46:15 +0100

    remove not used path
    Signed-off-by: Ahmad Kadri <ahmad.kadri@nordeck.net>

--- packages/react-sdk/src/components/Whiteboard/Draft/DraftMouseHandler.tsx
+++ packages/react-sdk/src/components/Whiteboard/Draft/DraftMouseHandler.tsx
@@ -59,8 +59,6 @@ export function DraftMouseHandler({
   const { activeTool } = useLayoutState();
   const shapeCursor = getCursorStyle(activeTool);
 
-  <path d="M2.99 3 3 21h12l6-6V3zM7 8h10v2H7zm5 6H7v-2h5zm2 5.5V14h5.5z"></path>;
-
   const handleClick = useCallback(
     (ev: MouseEvent<SVGRectElement>) => {
       if (onClick) {
