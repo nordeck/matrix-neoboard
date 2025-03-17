d2a45d02da0352b77bedad4bb2d4199e35f6d871
Ahmad Kadri <ahmad.kadri@nordeck.net>
2025-03-17 12:04:49 +0100

    remove the the padding from the canvas
    Signed-off-by: Ahmad Kadri <ahmad.kadri@nordeck.net>

--- packages/react-sdk/src/components/Whiteboard/WhiteboardHost.tsx
+++ packages/react-sdk/src/components/Whiteboard/WhiteboardHost.tsx
@@ -92,9 +92,7 @@ const WhiteboardHost = ({
       alignContent="space-around"
       position="relative"
       data-guided-tour-target="canvas"
-      {...(infiniteCanvasMode
-        ? { width: 'calc(100vw - 5px)', overflow: 'hidden' }
-        : {})}
+      {...(infiniteCanvasMode ? { width: '100vw', overflow: 'hidden' } : {})}
     >
       <SvgCanvas
         ref={svgRef}
