858ed0db1e1a8f7fd91d773dc8097774d4ea4a62
maheichyk <mikhail.aheichyk@gmail.com>
2025-05-30 15:09:30 +0300

    Update packages/react-sdk/src/components/ElementOverridesProvider/useGetElementOverride.ts
    Co-authored-by: Milton Moura <miltonmoura@gmail.com>
    Signed-off-by: maheichyk <mikhail.aheichyk@gmail.com>

--- packages/react-sdk/src/components/ElementOverridesProvider/useGetElementOverride.ts
+++ packages/react-sdk/src/components/ElementOverridesProvider/useGetElementOverride.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Nordeck IT + Consulting GmbH
+ * Copyright 2025 Nordeck IT + Consulting GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
