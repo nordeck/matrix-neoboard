15ff39134c83b81f72e91b8193a3eb6e9a4136db
MTRNord <213429047+Marcel-Nordeck@users.noreply.github.com>
2025-08-11 17:51:27 +0200

    Fix the usePowerLevels hook with temporary functions until the toolkit PR is being merged to export those types
    Signed-off-by: MTRNord <213429047+Marcel-Nordeck@users.noreply.github.com>

--- packages/react-sdk/src/store/api/usePowerLevels.ts
+++ packages/react-sdk/src/store/api/usePowerLevels.ts
@@ -15,13 +15,17 @@
  */
 
 import {
+  calculateUserPowerLevel,
   hasStateEventPower,
-  PowerLevelsStateEvent,
   STATE_EVENT_POWER_LEVELS,
+  UserPowerLevelType,
 } from '@matrix-widget-toolkit/api';
 import { useWidgetApi } from '@matrix-widget-toolkit/react';
 import { STATE_EVENT_WHITEBOARD } from '../../model';
-import { useGetPowerLevelsQuery } from './powerLevelsApi';
+import {
+  useGetCreateEventQuery,
+  useGetPowerLevelsQuery,
+} from './powerLevelsApi';
 
 export type PowerLevels = {
   canInitializeWhiteboard: boolean | undefined;
@@ -32,7 +36,11 @@ export type PowerLevels = {
 export function usePowerLevels({
   userId,
 }: { userId?: string } = {}): PowerLevels {
-  const { data: powerLevels, isLoading } = useGetPowerLevelsQuery();
+  const { data: powerLevels, isLoading: isLoadingPowerLevels } =
+    useGetPowerLevelsQuery();
+  const { data: createEvent, isLoading: isLoadingCreateEvent } =
+    useGetCreateEventQuery();
+  const isLoading = isLoadingPowerLevels || isLoadingCreateEvent;
   const widgetApi = useWidgetApi();
   userId ??= widgetApi.widgetParameters.userId;
 
@@ -42,12 +50,27 @@ export function usePowerLevels({
 
   if (!isLoading && powerLevels) {
     const powerContent = powerLevels.event?.content;
-    const userPowerLevel = getUserPowerLevel(userId, powerContent) ?? 0;
-    const canModerate = userPowerLevel >= 50;
+    const userPowerLevel =
+      calculateUserPowerLevel(powerContent, createEvent?.event, userId ?? '') ??
+      0;
+    const canModerate = compareUserPowerLevelToNormalPowerLevel(
+      userPowerLevel,
+      50,
+    );
 
     canInitializeWhiteboard =
-      hasStateEventPower(powerContent, userId, STATE_EVENT_POWER_LEVELS) &&
-      hasStateEventPower(powerContent, userId, STATE_EVENT_WHITEBOARD);
+      hasStateEventPower(
+        powerContent,
+        createEvent?.event,
+        userId,
+        STATE_EVENT_POWER_LEVELS,
+      ) &&
+      hasStateEventPower(
+        powerContent,
+        createEvent?.event,
+        userId,
+        STATE_EVENT_WHITEBOARD,
+      );
     canImportWhiteboard = canModerate;
     canStopPresentation = canModerate;
   }
@@ -59,16 +82,22 @@ export function usePowerLevels({
   };
 }
 
-export const getUserPowerLevel = (
-  userId: string | undefined,
-  powerLevel: PowerLevelsStateEvent | undefined,
-): number | undefined => {
-  if (!powerLevel || !userId) {
-    return undefined;
+// TODO: We should fix toolkit to instead export this method and then use it from toolkit.
+const ROOM_VERSION_12_CREATOR = 'ROOM_VERSION_12_CREATOR';
+
+// TODO: We should fix toolkit to instead export this method and then use it from toolkit.
+function compareUserPowerLevelToNormalPowerLevel(
+  userPowerLevel: UserPowerLevelType,
+  normalPowerLevel: number,
+): boolean {
+  if (userPowerLevel === ROOM_VERSION_12_CREATOR) {
+    // Room version 12 creator has the highest power level.
+    return true;
   }
-  if (powerLevel?.users && userId in powerLevel.users) {
-    return powerLevel?.users[userId];
-  } else {
-    return powerLevel.users_default;
+  if (typeof userPowerLevel !== 'number') {
+    // If the user power level is not a number, we cannot compare it to a normal power level.
+    return false;
   }
-};
+  // Compare the user power level to the normal power level.
+  return userPowerLevel >= normalPowerLevel;
+}
