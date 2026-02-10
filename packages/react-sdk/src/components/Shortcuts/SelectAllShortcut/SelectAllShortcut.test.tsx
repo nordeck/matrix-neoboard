d970d9b75b030a44a573846b93a257d8f48db287
nurjinn jafar <nurjin.jafar@nordeck.net>
2026-02-10 12:25:41 +0100

    add 'Select all' shortcut
    Signed-off-by: nurjinn jafar <nurjin.jafar@nordeck.net>

---
+++ packages/react-sdk/src/components/Shortcuts/SelectAllShortcut/SelectAllShortcut.test.tsx
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2026 Nordeck IT + Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { MockedWidgetApi, mockWidgetApi } from '@matrix-widget-toolkit/testing';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ComponentType, PropsWithChildren } from 'react';
+import { Mocked, afterEach, beforeEach, describe, expect, it } from 'vitest';
+import {
+  WhiteboardTestingContextProvider,
+  mockEllipseElement,
+  mockFrameElement,
+  mockLineElement,
+  mockWhiteboardManager,
+} from '../../../lib/testUtils';
+import { WhiteboardInstance, WhiteboardManager } from '../../../state';
+import { WhiteboardHotkeysProvider } from '../../WhiteboardHotkeysProvider';
+import { SelectAllShortcut } from './SelectAllShortcut';
+
+let widgetApi: MockedWidgetApi;
+
+afterEach(() => widgetApi.stop());
+
+beforeEach(() => {
+  widgetApi = mockWidgetApi();
+});
+
+describe('<SelectAllShortcut>', () => {
+  let Wrapper: ComponentType<PropsWithChildren<{}>>;
+  let whiteboardManager: Mocked<WhiteboardManager>;
+  let activeWhiteboardInstance: WhiteboardInstance;
+
+  beforeEach(() => {
+    ({ whiteboardManager } = mockWhiteboardManager({
+      slides: [
+        [
+          'slide-0',
+          [
+            ['element-0', mockLineElement()],
+            ['element-1', mockEllipseElement()],
+            [
+              'frame-0',
+              mockFrameElement({
+                position: { x: 500, y: 500 },
+                width: 300,
+                height: 300,
+              }),
+            ],
+          ],
+        ],
+      ],
+    }));
+    activeWhiteboardInstance = whiteboardManager.getActiveWhiteboardInstance()!;
+
+    Wrapper = ({ children }) => (
+      <WhiteboardHotkeysProvider>
+        <WhiteboardTestingContextProvider
+          whiteboardManager={whiteboardManager}
+          widgetApi={widgetApi}
+        >
+          {children}
+        </WhiteboardTestingContextProvider>
+      </WhiteboardHotkeysProvider>
+    );
+  });
+
+  it.each(['{Control>}a{/Control}', '{meta>}a'])(
+    'selects all elements with %s',
+    async (key) => {
+      const activeSlide = activeWhiteboardInstance.getSlide('slide-0');
+
+      render(<SelectAllShortcut />, { wrapper: Wrapper });
+
+      await userEvent.keyboard(key);
+
+      expect(activeSlide.getActiveElementIds()).toEqual(
+        activeSlide.getElementIds(),
+      );
+    },
+  );
+});
