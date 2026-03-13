523282909cff1c3b7278c14f5c7bbf8913b20f25
segnord <sergey.vasiluk@nordeck.net>
2026-03-13 22:55:48 +0300

    feat: Added the single object rotation feature with the corresponding feature flag REACT_APP_ENABLE_ROTATION
    Signed-off-by: segnord <sergey.vasiluk@nordeck.net>

--- packages/react-sdk/src/components/Whiteboard/ElementBehaviors/Resizable/utils.ts
+++ packages/react-sdk/src/components/Whiteboard/ElementBehaviors/Resizable/utils.ts
@@ -21,8 +21,10 @@ import {
   PathElement,
   Point,
 } from '../../../../state';
+import { isRotateableElement } from '../../../../state/crdt/documents/elements';
 import { ElementOverrideUpdate } from '../../../ElementOverridesProvider';
 import { snapToGrid } from '../../Grid';
+import { rotatePoint } from '../Rotatable/rotatorMath';
 import { DragEvent } from './ResizeHandle';
 import {
   Dimensions,
@@ -37,6 +39,51 @@ export function isLineElementHandlePosition(
   handlePosition: HandlePosition,
 ): handlePosition is LineElementHandlePosition {
   return handlePosition.name === 'start' || handlePosition.name === 'end';
+}
+
+function computeDragWithRotation(
+  event: DragEvent,
+  elements: Elements,
+  viewportWidth: number,
+  viewportHeight: number,
+  gridCellSize?: number,
+) {
+  const elementIds = Object.entries(elements).map((o) => o[0]);
+
+  // at this time we can only resize 1 rotated selected element properly
+  if (elementIds.length === 1) {
+    const [_elementId, element] = Object.entries(elements)[0];
+    if (isRotateableElement(element)) {
+      // rotate the cursor as if it was dragging the non-rotated handle
+      const angle = element.rotation ?? 0;
+      const cursor = { x: event.x, y: event.y };
+      const rotatedCursor = rotatePoint(
+        cursor,
+        {
+          x: element.position.x + element.width / 2,
+          y: element.position.y + element.height / 2,
+        },
+        -angle,
+      );
+
+      const rawDragX = clamp(rotatedCursor.x, 0, viewportWidth);
+      const rawDragY = clamp(rotatedCursor.y, 0, viewportHeight);
+
+      const dragX =
+        gridCellSize === undefined
+          ? rawDragX
+          : snapToGrid(rawDragX, gridCellSize);
+      const dragY =
+        gridCellSize === undefined
+          ? rawDragY
+          : snapToGrid(rawDragY, gridCellSize);
+
+      return { dragX, dragY };
+    }
+  }
+
+  // no rotations involved
+  return computeDrag(event, viewportWidth, viewportHeight, gridCellSize);
 }
 
 function computeDrag(
@@ -119,6 +166,7 @@ export function calculateDragDimension(
 
 export function calculateDimensions(
   handlePositionName: HandlePositionName,
+  elementsPossiblyRotated: Elements,
   event: DragEvent,
   startDimensions: Dimensions,
   viewportWidth: number,
@@ -126,8 +174,9 @@ export function calculateDimensions(
   invertLockAspectRatio: boolean = false,
   gridCellSize?: number,
 ): Dimensions {
-  const { dragX, dragY } = computeDrag(
+  const { dragX, dragY } = computeDragWithRotation(
     event,
+    elementsPossiblyRotated,
     viewportWidth,
     viewportHeight,
     gridCellSize,
@@ -308,8 +357,9 @@ export function computeResizing(
   }
 
   if (isLineElementHandlePosition(handlePosition)) {
-    const { dragX, dragY } = computeDrag(
+    const { dragX, dragY } = computeDragWithRotation(
       event,
+      resizableProperties.elements,
       viewportWidth,
       viewportHeight,
       gridCellSize,
@@ -342,6 +392,7 @@ export function computeResizing(
 
   const dimensions = calculateDimensions(
     handlePosition.name,
+    resizableProperties.elements,
     event,
     resizableProperties,
     viewportWidth,
@@ -350,59 +401,135 @@ export function computeResizing(
     gridCellSize,
   );
 
-  const res: ElementOverrideUpdate[] = Object.entries(
-    resizableProperties.elements,
-  ).map(([elementId, element]) => {
-    return {
-      elementId,
-      elementOverride: {
-        position: {
+  const resizeInfo: {
+    elementOverride: ElementOverrideUpdate;
+    elementId: string;
+    pointResizer: (p: Point) => Point;
+  }[] = Object.entries(resizableProperties.elements).map(
+    ([elementId, element]) => {
+      const pointResizerUnrotated = (p: Point) => {
+        const resized = {
           x:
             dimensions.x +
-            ((element.position.x - resizableProperties.x) /
-              resizableProperties.width) *
+            ((p.x - resizableProperties.x) / resizableProperties.width) *
               dimensions.width,
           y:
             dimensions.y +
-            ((element.position.y - resizableProperties.y) /
-              resizableProperties.height) *
+            ((p.y - resizableProperties.y) / resizableProperties.height) *
               dimensions.height,
+        };
+        return resized;
+      };
+
+      const override = {
+        elementId,
+        elementOverride: {
+          position: pointResizerUnrotated({
+            x: element.position.x,
+            y: element.position.y,
+          }),
+          width:
+            element.type === 'shape' ||
+            element.type === 'image' ||
+            element.type === 'frame'
+              ? (element.width / resizableProperties.width) * dimensions.width
+              : undefined,
+          height:
+            element.type === 'shape' ||
+            element.type === 'image' ||
+            element.type === 'frame'
+              ? (element.height / resizableProperties.height) *
+                dimensions.height
+              : undefined,
+          points:
+            element.type === 'path'
+              ? element.points.map((point) => ({
+                  x: (point.x / resizableProperties.width) * dimensions.width,
+                  y: (point.y / resizableProperties.height) * dimensions.height,
+                }))
+              : undefined,
         },
-        width:
-          element.type === 'shape' ||
-          element.type === 'image' ||
-          element.type === 'frame'
-            ? (element.width / resizableProperties.width) * dimensions.width
-            : undefined,
-        height:
-          element.type === 'shape' ||
-          element.type === 'image' ||
-          element.type === 'frame'
-            ? (element.height / resizableProperties.height) * dimensions.height
-            : undefined,
-        points:
-          element.type === 'path'
-            ? element.points.map((point) => ({
-                x: (point.x / resizableProperties.width) * dimensions.width,
-                y: (point.y / resizableProperties.height) * dimensions.height,
-              }))
-            : undefined,
-      },
-    };
-  });
+      };
+
+      let pointResizerRotated = undefined;
+
+      if (isRotateableElement(element)) {
+        // Resizing an object shifts it's center of rotation.
+        // Apply adjustment to the offsets of the non rotated object
+        // to eliminate the visible shift.
+        const origCenter = {
+          x: element.position.x + element.width / 2,
+          y: element.position.y + element.height / 2,
+        };
+        const newCenter = {
+          x: override.elementOverride.position.x + dimensions.width / 2,
+          y: override.elementOverride.position.y + dimensions.height / 2,
+        };
+
+        // calculate "rotation center shift error" by selecting a
+        // point and rotating it around both centers.
+        // Used to shift the object (and it's new center of rotation)
+        // because some points were not affected by the resize and should remain
+        // in their original place.
+        const testPoint = {
+          x: element.position.x + element.width,
+          y: element.position.y + element.height,
+        };
+        const angle = element.rotation ?? 0;
+        const p1 = rotatePoint(testPoint, origCenter, angle);
+        const p2 = rotatePoint(testPoint, newCenter, angle);
+        const error = {
+          x: p1.x - p2.x,
+          y: p1.y - p2.y,
+        };
+        override.elementOverride.position.x += error.x;
+        override.elementOverride.position.y += error.y;
+
+        // this function will predict where a point on this object will be
+        // after applying the resize (in SVG coordinates)
+        // it unrotates the point, applies the "resize logic" and
+        // rotates the point over the new center applying the error shift.
+        pointResizerRotated = (p: Point) => {
+          const u = rotatePoint(p, origCenter, -angle);
+          const resized = pointResizerUnrotated({
+            x: u.x,
+            y: u.y,
+          });
+          const n = rotatePoint(resized, newCenter, angle);
+          n.x += error.x;
+          n.y += error.y;
+          return n;
+        };
+      }
+
+      return {
+        elementOverride: override,
+        pointResizer: pointResizerRotated ?? pointResizerUnrotated,
+        elementId,
+      };
+    },
+  );
+
+  const elementOverrides = resizeInfo.map((o) => o.elementOverride);
+
+  const resizers: Record<string, (p: Point) => Point> = {};
+  for (const o of resizeInfo) {
+    if (o.pointResizer) resizers[o.elementId] = o.pointResizer;
+  }
 
   if (resizableProperties.connectingPathElements) {
-    res.push(
+    elementOverrides.push(
       ...computeResizingConnectingPathElements(
         resizableProperties.connectingPathElements,
         resizableProperties.elements,
         resizableProperties,
         dimensions,
+        resizers,
       ),
     );
   }
 
-  return res;
+  return elementOverrides;
 }
 
 export function computeResizingConnectingPathElements(
@@ -420,6 +547,7 @@ export function computeResizingConnectingPathElements(
     width: number;
     height: number;
   },
+  resizers?: Record<string, (p: Point) => Point>,
 ): ElementOverrideUpdate[] {
   const res: ElementOverrideUpdate[] = [];
 
@@ -432,6 +560,7 @@ export function computeResizingConnectingPathElements(
       elements,
       resizableProperties,
       dimensions,
+      resizers,
     );
     if (eos) {
       res.push(eos);
@@ -462,6 +591,7 @@ function computeResizingConnectingPathElement(
     width: number;
     height: number;
   },
+  resizers?: Record<string, (p: Point) => Point>,
 ): ElementOverrideUpdate | undefined {
   if (!(element.connectedElementStart || element.connectedElementEnd)) {
     return undefined;
@@ -479,25 +609,42 @@ function computeResizingConnectingPathElement(
   for (let i = 0; i < connectedElements.length; i++) {
     const connectedElementId = connectedElements[i];
     if (connectedElementId && elements[connectedElementId] !== undefined) {
+      const elementWithConnectorPoints = elements[connectedElementId];
       const pointX = element.position.x + element.points[i].x;
       const pointY = element.position.y + element.points[i].y;
 
-      const newPointX =
-        dimensions.x +
-        ((pointX - resizableProperties.x) / resizableProperties.width) *
-          dimensions.width;
-
-      const newPointY =
-        dimensions.y +
-        ((pointY - resizableProperties.y) / resizableProperties.height) *
-          dimensions.height;
+      const drag = {
+        x:
+          dimensions.x +
+          ((pointX - resizableProperties.x) / resizableProperties.width) *
+            dimensions.width,
+        y:
+          dimensions.y +
+          ((pointY - resizableProperties.y) / resizableProperties.height) *
+            dimensions.height,
+      };
 
       const positionName = i === 0 ? 'start' : 'end';
+
+      // predict where a connection point will be for a rotated
+      // object using the object's own point rotation function
+      if (
+        resizers &&
+        resizers[connectedElementId] &&
+        isRotateableElement(elementWithConnectorPoints) &&
+        elementWithConnectorPoints.rotation
+      ) {
+        const resizeFn = resizers[connectedElementId];
+        const resizedPoint = resizeFn({ x: pointX, y: pointY });
+        drag.x = resizedPoint.x;
+        drag.y = resizedPoint.y;
+      }
+
       linePosition = computeResizingOfLine(
         positionName,
         linePosition,
-        newPointX,
-        newPointY,
+        drag.x,
+        drag.y,
       );
     }
   }
