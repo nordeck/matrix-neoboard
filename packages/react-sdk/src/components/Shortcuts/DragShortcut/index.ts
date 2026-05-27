02aa285b6ff58d68dcd326c6a486905323ba3613
nurjinn jafar <nurjin.jafar@nordeck.net>
2026-05-27 12:27:05 +0200

    update copyright year
    Signed-off-by: nurjinn jafar <nurjin.jafar@nordeck.net>

--- packages/react-sdk/src/components/Shortcuts/DragShortcut/index.ts
+++ packages/react-sdk/src/components/Shortcuts/DragShortcut/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Nordeck IT + Consulting GmbH
+ * Copyright 2026 Nordeck IT + Consulting GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
